b8138641fe3f9de6279ab930e4ade00324178380
kfoxb <kfoxb@kfoxb.com>
2017-02-17 15:57:00 -0800

    Implement init, send, and fetch functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,37 @@
 // YOUR CODE HERE:
+var app = {};
+app.init = function() {
+  return true;
+};
+
+app.success = function() {
+  console.log('success');
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    datatype: 'json',
+    success: null,
+    data: JSON.stringify(message)
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: undefined,
+    type: 'GET',
+    datatype: 'json',
+    success: null,
+    data: JSON.stringify(message)
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').children().remove();
+};
+
+app.renderMessage = function() {
+
+};

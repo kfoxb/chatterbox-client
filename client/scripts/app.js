f302b50dbb86dd22e6837044461e239203c3b96d
Kyle Bradford <kfoxb@kfoxb.com>
2017-02-19 02:54:48 -0800

    refactor app.resetToCorrectRoom and app.refresh into smaller functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,24 +4,37 @@ var app = {};
 app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
 
 app.init = function() {
-  app.fetch(); 
+  app.fetch((data) => {
+    app.appendFetchtoStorage(data);
+    app.initialize();
+  }); 
   app.handleUsernameClick();
   app.handleSubmit();
-  app.filterRooms();
+  app.changeRoom();
   app.createRoom();
-  setInterval(app.resetToCorrectRoom, 2000);
+  //setInterval(app.fetch, 2000);
+};
+
+app.initialize = () => {
+  //move this functionality into appendFetchtoStorage
+  for (let message in app.messageStorage) {
+    app.roomList.add(app.messageStorage[message].roomname);
+  }
+  //delete all menu children before this loop runs
+  for (let room of app.roomList) {
+    app.renderRoom(room);
+  }
+
+  app.generateRoom();
 };
 
 app.messageStorage = {};
 app.friendList = {};
-
-app.success = function() {
-  console.log('success');
-};
+app.roomList = new Set();
 
 app.send = function(message) {
   $.ajax({
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+    url: app.server,
     type: 'POST',
     contentType: 'application/json',
     datatype: 'json',
@@ -30,31 +43,27 @@ app.send = function(message) {
   });
 };
 
-var firstTimeRun = true;
-
-app.fetch = function(cb) {
+app.fetch = (cb = appendFetchtoStorage) => {
   $.ajax({
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     data: {
       order: '-createdAt'
     },
     type: 'GET',
     datatype: 'json',
     contentType: 'application/json',
-    success: function(data) {
-      for (var i = 0; i < data.results.length; i++) {
-        app.messageStorage[data.results[i].objectId] = app.escapeMessage(data.results[i]);
-      }
-      if (firstTimeRun) {
-        app.refresh();
-        firstTimeRun = false;  
-      }
-    },
+    success: cb,
     failure: function(data) {
       console.log('this failed:' + data);
     }
   });
 };
+
+app.appendFetchtoStorage = function(data) {
+  for (var i = 0; i < data.results.length; i++) {
+    app.messageStorage[data.results[i].objectId] = app.escapeMessage(data.results[i]);
+  }
+},
 
 app.clearMessages = function() {
   $('#chats').children().remove();
@@ -85,8 +94,6 @@ app.escapeMessage = function(message) {
 };
 
 app.renderMessage = function(message, appendOrPrepend = 'append') {
-  // debugger;
-  message = app.escapeMessage(message);
   var text = '<div>' + message.text + '</div>';
   var username = '<a class="username" href="#">' + message.username + '</a>';
   var roomName = '<div>' + message.roomname + '</div>';
@@ -95,20 +102,15 @@ app.renderMessage = function(message, appendOrPrepend = 'append') {
     text = '<strong>' + text + '</strong>';
   }
   var completeMessage = '<div>' + username + text + roomName + '</div>';
-  //$('#main').append(username);
   $('#chats')[appendOrPrepend](completeMessage);
 };
 
-app.renderRoom = function (roomName) {
-  var room = '<option>' + roomName + '</option>';
-  $('#roomSelect').append(room);
-};
 
 app.handleUsernameClick = function() {
   $('#chats').on('click', '.username', function() {
     var username = $(this).text();
     app.friendList[username] = username;
-    app.resetToCorrectRoom();
+    app.generateRoom();
   });
 };
 
@@ -121,44 +123,27 @@ app.handleSubmit = function() {
     message.roomname = $(':selected').val();
     app.send(message);
     $('input[name=messageForm]').val('');
-    // app.resetToCorrectRoom();
     app.renderMessage(message, 'prepend');
   });
 };
 
-app.refresh = function() {
-  // app.fetch();
-  //clears all chats off screen
-  $('#chats').children().remove();
-  var initializeRooms = function(storage) {
-    var roomList = {};
-    for (var key in storage) {
-      var roomName = storage[key].roomname;
-      roomList[roomName] = roomName;
-    }
-
-    for (key in roomList) {
-      app.renderRoom(roomList[key]);
-    }
-  };
-  
-  var initializeMessages = function(storage) {
-    for (key in storage) {
-      app.renderMessage(storage[key]);
-    }
-  };
-
-  initializeRooms(app.messageStorage);
-  initializeMessages(app.messageStorage);
+app.changeRoom = function() {
+  $('#roomSelect').on('change', app.generateRoom);
 };
 
-// handler for our drop down menu to hide other rooms
-app.filterRooms = function () {
-  $('#roomSelect').on('change', app.resetToCorrectRoom);
+app.createRoom = function() {
+  $('#createRoom form').submit(function(event) {
+    event.preventDefault();
+    let roomname = $('input[name=createRoomForm]').val();
+    app.renderRoom(roomname);
+    $('input[name=createRoomForm]').val('');
+    $('#roomSelect').val(roomname);
+    app.generateRoom();
+  });
 };
 
-app.resetToCorrectRoom = function() {
-  $('#chats').children().remove();
+app.generateRoom = () => {
+  app.clearMessages();
   for (var key in app.messageStorage) {
     if (app.messageStorage[key].roomname === $(':selected').val()) {
       app.renderMessage(app.messageStorage[key]);
@@ -166,17 +151,47 @@ app.resetToCorrectRoom = function() {
   }
 };
 
-app.createRoom = function() {
-  $('#createRoom form').submit(function(event) {
-    event.preventDefault();
-    var message = {};
-    message.roomname = $('input[name=createRoomForm]').val();
-    app.messageStorage[message.roomname] = message.roomname;
-    $('input[name=createRoomForm]').val('');
-    app.renderRoom(message.roomname);
-    $('#roomSelect').val(message.roomname);
-    app.resetToCorrectRoom();
-  });
+app.renderRoom = function(roomName) {
+  var room = '<option>' + roomName + '</option>';
+  $('#roomSelect').append(room);
 };
+
+// app.refresh = function() {
+//   // app.fetch();
+//   //clears all chats off screen
+//   $('#chats').children().remove();
+//   var initializeRooms = function(storage) {
+//     var roomList = {};
+//     for (var key in storage) {
+//       var roomName = storage[key].roomname;
+//       roomList[roomName] = roomName;
+//     }
+
+//     for (key in roomList) {
+//       app.renderRoom(roomList[key]);
+//     }
+//   };
+  
+//   var initializeMessages = function(storage) {
+//     for (key in storage) {
+//       app.renderMessage(storage[key]);
+//     }
+//   };
+
+//   initializeRooms(app.messageStorage);
+//   initializeMessages(app.messageStorage);
+// };
+
+// handler for our drop down menu to hide other rooms
+
+// app.resetToCorrectRoom = function() {
+//   $('#chats').children().remove();
+//   for (var key in app.messageStorage) {
+//     if (app.messageStorage[key].roomname === $(':selected').val()) {
+//       app.renderMessage(app.messageStorage[key]);
+//     }
+//   }
+// };
+
 
 $('document').ready(app.init);
